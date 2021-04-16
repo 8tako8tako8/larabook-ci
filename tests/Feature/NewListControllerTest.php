dffc46c4de4562cde3fb8d6035993561e9427b62
森本晃輔 <8tako8tako8@MacBook-Pro.local>
2021-04-17 04:03:08 +0900

    fix: テストに失敗するように変更

--- tests/Feature/NewListControllerTest.php
+++ tests/Feature/NewListControllerTest.php
@@ -14,7 +14,7 @@ class NewListControllerTest extends TestCase
     {
         $response = $this->get(route('newlist.index'));
 
-        $response->assertStatus(200)
+        $response->assertStatus(400)
             ->assertViewIs('newlist.index');
     }
 }
